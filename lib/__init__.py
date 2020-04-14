b421bd90ece4fecaf511682b3cf3765f1584bf76
Ulti-Dreisteine <dreisteine262@163.com>
2020-04-14 18:19:49 +0800

    加入偏导数值和符号计算

---
+++ lib/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020/4/14 18:18
+
+@Project -> File: numerical-optimization -> __init__.py
+
+@Author: luolei
+
+@Email: dreisteine262@163.com
+
+@Describe:
+"""
+
+import logging
+
+logging.basicConfig(level = logging.INFO)
+
+import sys, os
+
+sys.path.append('../')
+
+from mod.mathematics.partial_derives import NumPartialDerives
+
+
+
---
+++ lib/linear_search.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020/4/14 18:17
+
+@Project -> File: numerical-optimization -> linear_search.py
+
+@Author: luolei
+
+@Email: dreisteine262@163.com
+
+@Describe:
+"""
+
+import logging
+
+logging.basicConfig(level = logging.INFO)
+
+import sys, os
+
+sys.path.append('../')
+
+from lib import NumPartialDerives
+
+if __name__ == '__main__':
+	pass
+
+
+
